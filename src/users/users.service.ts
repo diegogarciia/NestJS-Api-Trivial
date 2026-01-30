cc843a9da84dd57cce41bc8e6e6131bd7fa4e3bd
Diego <diegogarciaangel25@gmail.com>
2026-01-30 18:04:01 +0100

    primeras pruebas

---
+++ src/users/users.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  private users : User[]= [
+    {
+      id: 1,
+      name: 'Diego García Ángel',
+      email: "diego@example.com",
+      password: "123"
+    },
+    {
+      id: 2,
+      name: 'Ian Gabriel Castellanos',
+      email: "ian@example.com",
+      password: "123"
+    }
+  ];
+
+  private generateUniqueId = () => {
+    let newId;
+    do {
+      newId = Math.floor(Math.random() * 1000) + 1;
+    } while (this.users.some(user => user.id === newId));
+    return newId;
+  };
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
