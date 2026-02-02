ffd5c3782a671ff3d0c47eda57f961f5c201ce7c
Diego <diegogarciaangel25@gmail.com>
2026-02-02 12:07:55 +0100

    create post

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsOptional, IsString, IsBoolean, IsInt } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  published?: boolean;
+
+  @IsInt()
+  @IsOptional()
+  authorId?: number; 
+}
