7a5be1338ca514c4ea59c8dfb597e355d27b0b78
Diego <diegogarciaangel25@gmail.com>
2026-02-07 11:21:00 +0100

    ajustes cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import kleur from 'kleur';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
   console.log(
